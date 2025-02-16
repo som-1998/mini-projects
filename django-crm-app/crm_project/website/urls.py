3a60bbeb242902482381241200b79541efa8d4e8
som-1998 <somchakraborty98@gmail.com>
2025-02-16 20:44:24 +0530

    registrer

--- django-crm-app/crm_project/website/urls.py
+++ django-crm-app/crm_project/website/urls.py
@@ -1,1 +1,10 @@
-## web urls
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home , name = "home"),
+    # path('login/', views.login_user , name = "login"),
+    path('logout', views.logout_user , name = "logout"),
+    path('register', views.register_user , name = "register"),
+]
